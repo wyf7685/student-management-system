3a8a9780567d22488a706c6a7bd8e3a1c01a2b28
liaochenjun <1613603747@qq.com>
2025-01-16 14:11:57 +0800

    奖项update

--- src/const.py
+++ src/const.py
@@ -3,7 +3,7 @@ from pathlib import Path
 VERSION = "0.1.0"
 AUTHORS = (
     ("wyf7685", "wyf7685@163.com"),
-    ("lcj", "xxx@qq.com"),
+    ("lcj", "1613603747@qq.com"),
     ("wxy", "xxx@qq.com"),
 )
 COPYRIGHT = "© 2025 All Rights Reserved"
--- src/ui/student/pages/award.py
+++ src/ui/student/pages/award.py
@@ -2,7 +2,7 @@ from ui.common.page import BasePage
 
 
 class AwardPage(BasePage):
-    button_name = "课表查询"
+    button_name = "获奖查询"
 
     def init_ui(self):
         pass
