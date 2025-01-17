5ee2a37c52d415ed64320be858eda6fa87597156
wyf7685 <wyf7685@163.com>
2025-01-17 15:30:14 +0800

    fix style

--- src/ui/teacher/window.py
+++ src/ui/teacher/window.py
@@ -1,3 +1,5 @@
+from PyQt6.QtWidgets import QHBoxLayout, QSpacerItem, QWidget
+
 from ui.common.user_window import BaseUserWindow
 
 from .pages import PAGES
@@ -6,3 +8,13 @@ from .pages import PAGES
 class TeacherMainWindow(BaseUserWindow):
     title = "学生信息管理系统 - 教师端"
     page_cls = PAGES
+
+    def add_stack_widget(self, widget: QWidget):
+        w = QWidget()
+        layout = QHBoxLayout()
+        w.setLayout(layout)
+        spacer = QSpacerItem(50, 20)
+        layout.addSpacerItem(spacer)
+        layout.addWidget(widget)
+        layout.addSpacerItem(spacer)
+        return super().add_stack_widget(w)
